a98046d3c7533304206b325a5ecde1b64d5ed47f
Jake Bouma <jake.t.bouma@gmail.com>
2025-06-09 11:09:51 +0200

    test and tidy

--- README.md
+++ README.md
@@ -7,6 +7,30 @@
 |:--------|:---------|
 | - Determine all solutions to the game<br>- Develop an API for set(pieces) -> set(solutions) | - UX with a 3D viewer of solutions<br>- An AI player (RL?) |
 
+### Algorithm 1.0
+Given a large set of game `blokks` that may be used to build a cube of dimension `cube_size` with perfect volume `cube_size**3`.
+
+Prep. Build the following data structures
+1. Generate all the subsets of `blokks` that have a combined voxel volume equal to the perfect volume. Base the solution on integer partitions. This is a sequential generation that doesn't parallelize well, so save these subsets to disk. They can then be used to parallelize and track/resume progress.
+1. Generate all possible rotations and translations of the blokk within the cube. Cache these.
+
+Parallel processing of each subset:
+1. Iterate through the cartesian product of all possible rotations and translations of each blokk in the subset
+   1. Add the blokks together.
+   2. If no voxels overlap, then this subset is a solution!
+   3. If any voxels overlap, this is not a solution. Continue searching.
+2. If you have processed the last element of the product, and haven't yet found a solution, then this subset is not a solution.
+3. Save the outcome to disk.
+
+### Running the project
+Prerequisites;
+* Python poetry
+* Optionally, duckdb cli
+
+1. Install with `poetry install`
+2. Build the samples `poetry run poe save-blokk-samples`
+3. ...
+
 ---
 [Kickstarter](https://www.kickstarter.com/projects/blokkgames/blokk-dare-to-be-square) | [Instagram](https://www.instagram.com/blokk.games) | [Get in touch to contribute](mailto:jake@honestgrowth.no)
 
--- scripts/results_database.py
+++ scripts/results_database.py
@@ -1,10 +1,8 @@
-import argparse
 import logging
 from itertools import batched
 
 import duckdb
 import polars as pl
-import pyarrow as pa
 from tqdm import tqdm
 
 from blokk_solver.combinatorics import generate_all_blokk_samples
@@ -19,11 +17,24 @@ def stream_blokk_samples_to_duckdb(
     max_volume=5,
     database_schema=None,
     restart=True,
+    loglevel="INFO",
 ):
+    logging.getLogger().setLevel(loglevel)
+
     if database_schema is None:
         database_schema = f"cube_{cube_size}"
     table_name = f"{database_schema}.samples_{max_volume}"
     sequence_name = f"{database_schema}.sq_samples_{max_volume}"
+
+    logger.info(
+        "\n==== stream_blokk_samples_to_duckdb ==============\n"
+        f"database        : {database}\n"
+        f"cube_size       : {cube_size}\n"
+        f"max_volume      : {max_volume}\n"
+        f"table_name      : {table_name}\n"
+        f"restart         : {restart}\n"
+        "===================================================="
+    )
 
     sample_generator = generate_all_blokk_samples(
         cube_volume=cube_size**3, max_volume=5
@@ -63,9 +74,9 @@ def stream_blokk_samples_to_duckdb(
 
     # Wrap batch enumeration with tqdm for progress tracking
     batch_size = 10000
-    logger.info(f"Processing batches of {batch_size} rows")
+    logger.debug(f"Processing batches of {batch_size} rows")
     for batch_idx, batch in enumerate(
-        tqdm(batched(sample_generator, batch_size), desc="Inserting batches")
+        tqdm(batched(sample_generator, batch_size), desc="\t⏩")
     ):
         # should really filter by blokk length < cube_length,
         # though that becomes unimportant at higher dimensions.
@@ -88,66 +99,11 @@ def stream_blokk_samples_to_duckdb(
             INSERT INTO {table_name} (integer_partition_idx, blokks, batch_idx)
             SELECT *, '{batch_idx}' as batch_idx FROM df
         """)
-    logger.info("Insertion completed successfully.")
 
     # After all batches, check row count and print
     result = con.sql(f"SELECT COUNT(*) FROM {table_name}").fetchone()
     logger.info(f"Inserted {result[0]} rows into {table_name}")
 
 
-def stream_blokk_samples_with_arrow(filename, cube_size, max_volume):
-    sample_generator = generate_all_blokk_samples(
-        cube_volume=cube_size**3, max_volume=5
-    )
-
-    schema = pa.schema(
-        [
-            pa.field("integer_partition_id", pa.int32()),
-            pa.field("blokk_ids", pa.list_(pa.int8())),
-        ]
-    )
-
-    local = fs.LocalFileSystem()
-    with local.open_output_stream(filename) as stream:
-        # with pa.ipc.RecordBatchStreamWriter(stream, schema) as writer:
-        with csv.CSVWriter(stream, schema) as writer:
-            for batch in batched(sample_generator, 10000):
-                integer_partition_ids, blokk_ids = list(
-                    zip(*batch)
-                )  # transpose list of rows to list of cols
-                blokk_ids = pa.array([list(x) for x in blokk_ids])
-                integer_partition_ids = pa.array(integer_partition_ids)
-                record_batch = pa.record_batch(
-                    [integer_partition_ids, blokk_ids],
-                    schema=schema,
-                )
-                writer.write_batch(record_batch)
-
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Generate and store blokk samples in DuckDB."
-    )
-    parser.add_argument(
-        "--cube-size", type=int, default=2, help="Cube size (default: 2)"
-    )
-    parser.add_argument(
-        "--max-volume", type=int, default=4, help="Max volume (default: 4)"
-    )
-    parser.add_argument(
-        "--log-level",
-        type=str,
-        default="INFO",
-        choices=["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"],
-        help="Set logging level (default: DEBUG)",
-    )
-    args = parser.parse_args()
-
-    logging.getLogger().setLevel(args.log_level.upper())
-    logger.setLevel(args.log_level.upper())
-
-    stream_blokk_samples_to_duckdb(
-        cube_size=args.cube_size,
-        max_volume=args.max_volume,
-        restart=True,
-    )
+    stream_blokk_samples_to_duckdb(loglevel="DEBUG")
